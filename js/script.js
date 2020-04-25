f209779c5c04a0ef1c450779cdd300f6c2becaf9
zw <1303710705@qq.com>
2020-04-25 13:06:16 +0800

    Site updated: 2020-04-25 13:06:13

--- js/script.js
+++ js/script.js
@@ -84,5 +84,13 @@ document.ready(
 
         // ready function.
 
+            [].slice.call(document.querySelectorAll('table')).forEach(function(el){
+    var wrapper = document.createElement('div');
+    wrapper.className = 'table-area';
+    el.parentNode.insertBefore(wrapper, el);
+    el.parentNode.removeChild(el);
+    wrapper.appendChild(el);
+        })
     }
+    
 );
